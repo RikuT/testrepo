2ed752553b6e3d986e2b9db54671d84c80993d7c
Kenty <Kenty@MacMini.local>
2015-06-26 22:32:06 +0900

    Initial Commit

---
+++ Stylist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stylist
+//
+//  Created by Kenty on 2015/06/26.
+//  Copyright (c) 2015年 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stylist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stylist
+//
+//  Created by Kenty on 2015/06/26.
+//  Copyright (c) 2015年 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
