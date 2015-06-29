d76844fd467f1c18e2a440a36184aa82d0c1943d
Kenty <Kenty@MacMini.local>
2015-06-29 22:39:19 +0900

    UPDATE Stylist Alpha 12
    ・upload and retrieve image from parse

--- Stylist/SingleRowCell.swift
+++ Stylist/SingleRowCell.swift
@@ -2,36 +2,25 @@
 //  SingleRowCell.swift
 //  Stylist
 //
-//  Created by Kento Katsumata on 2015/06/29.
+//  Created by Kenty on 2015/06/29.
 //  Copyright (c) 2015年 xxx. All rights reserved.
 //
 
 import UIKit
 
-class SingleRowCell: UIViewController {
+class SingleRowCell: UITableViewCell {
 
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var topsImageView: UIImageView!
+    @IBOutlet weak var topsLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        // Configure the view for the selected state
     }
-    */
 
 }
---
+++ Stylist/TopsViewController.swift
@@ -0,0 +1,84 @@
+//
+//  TopsViewController.swift
+//  Stylist
+//
+//  Created by Kenty on 2015/06/29.
+//  Copyright (c) 2015年 xxx. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TopsViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var topsTableView: UITableView!
+    //Create arrays of images from parse
+    var imageFiles = [PFFile]()
+    var imageText = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var query = PFQuery(className: "Posts")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock{
+            (posts: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                for post in posts!{
+                    self.imageFiles.append(post["imageFile"]as! PFFile)
+                    self.imageText.append(post["imageText"]as! String)
+            }
+                
+                
+            println(self.imageFiles.count)
+                self.topsTableView.reloadData()
+            
+        } else {
+                println(error)
+        }}}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+        //テーブルビューのコードかくかく、これがめんどい
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return imageFiles.count
+    
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let singleCell:SingleRowCell = tableView.dequeueReusableCellWithIdentifier("mySingleCell")as! SingleRowCell
+        //ケンティーのリマインドノート　↓ONLY label
+        singleCell.topsLabel.text = imageText [indexPath.row]
+        //イメージとってくるぜよ　もどってこーーーーい
+        imageFiles[indexPath.row].getDataInBackgroundWithBlock{
+          (imageData: NSData?, error: NSError?) -> Void in
+        if imageData != nil {
+            let image = UIImage(data: imageData!)
+            singleCell.topsImageView.image = image
+        }else {
+            println(error)
+            }}
+        
+        
+        return singleCell
+        }}
+
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
