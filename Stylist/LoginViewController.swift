7b4d659cedea5c5e9016e57c6a27a8d747124260
Kenty <Kenty@MacMini.local>
2015-08-17 03:49:12 +0900

    UPDATE Stylist alpha 58
    ・Tutorial implemented

--- Stylist/LoginViewController.swift
+++ Stylist/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
+class LoginViewController: UIViewController, UITextFieldDelegate,BWWalkthroughViewControllerDelegate {
     
     @IBOutlet var usernameField: MKTextField!
     @IBOutlet var passwordField: MKTextField!
@@ -17,13 +17,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var loginBtn: MKButton!
     @IBOutlet var signupBtn: MKButton!
     @IBOutlet weak var logoLabel: UILabel!
-
-
-
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         logoLabel.textColor = UIColor(red: 0, green: 0.698, blue: 0.792, alpha: 1)
         
         usernameField.layer.borderColor = UIColor.clearColor().CGColor
@@ -57,6 +56,38 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         signupBtn.layer.shadowColor = UIColor.grayColor().CGColor
         signupBtn.layer.shadowOffset = CGSize(width: 0, height: 2.5)
         // Do any additional setup after loading the view.
+        
+        
+        
+
+            }
+     override func viewDidAppear(animated: Bool) {
+        // Get view controllers and build the walkthrough
+        let stb = UIStoryboard(name: "Tutorial", bundle: nil)
+        let walkthrough = stb.instantiateViewControllerWithIdentifier("walk") as! BWWalkthroughViewController
+        let page_one = stb.instantiateViewControllerWithIdentifier("walk1") as! UIViewController
+        let page_two = stb.instantiateViewControllerWithIdentifier("walk2")as! UIViewController
+        let page_three = stb.instantiateViewControllerWithIdentifier("walk3") as! UIViewController
+        let page_four = stb.instantiateViewControllerWithIdentifier("walk4")as! UIViewController
+        
+        // Attach the pages to the master
+        walkthrough.delegate = self
+        walkthrough.addViewController(page_one)
+        walkthrough.addViewController(page_two)
+        walkthrough.addViewController(page_three)
+        walkthrough.addViewController(page_four)
+        self.presentViewController(walkthrough, animated: true, completion: nil)
+
+    }
+    
+    // MARK: - Walkthrough delegate -
+    
+    func walkthroughPageDidChange(pageNumber: Int) {
+        println("Current Page \(pageNumber)")
+    }
+    
+    func walkthroughCloseButtonPressed() {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -70,7 +101,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         return true;
         
     }
-//ログインボタンがタップされたら以下のコードが実行されます。
+    //ログインボタンがタップされたら以下のコードが実行されます。
     @IBAction func loginButtonTapped(sender: AnyObject) {
         let userUserName = usernameField.text
         let userPassword = passwordField.text
@@ -86,13 +117,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 
             }
                 
-        //ユーザーネームとパスワードが一致しない場合
+                //ユーザーネームとパスワードが一致しない場合
             else{
-
+                
                 println("User Name or Password is not correct")
                 let errorAlert = SCLAlertView()
                 errorAlert.showError("Error", subTitle:"Username of Password is not correct.", closeButtonTitle:"Close")
-
+                
             }
             
             
